ffde15f606ee9d8169871852dbd8b0a97779513b
Erin Pentecost <erin@pentecost.email>
2026-06-11 17:50:08 -0700

    fade gold and clock

--- scripts/clock.lua
+++ scripts/clock.lua
@@ -1,56 +1,66 @@
-local ui = require('openmw.ui')
-local util = require('openmw.util')
-local calendar = require('openmw_aux.calendar')
-local time = require('openmw_aux.time')
-local MWUI = require('openmw.interfaces').MWUI
+local ui               = require('openmw.ui')
+local util             = require('openmw.util')
+local calendar         = require('openmw_aux.calendar')
+local time             = require('openmw_aux.time')
+local MWUI             = require('openmw.interfaces').MWUI
 
-local cornerMargin = 36
-local hOffset, vOffset  = -45, 1
-local white = ui.texture { path = 'white' }
+local cornerMargin     = 36
+local hOffset, vOffset = -45, 1
+local white            = ui.texture { path = 'white' }
 
-local widget = {
-	type = ui.TYPE.Image,
-	props = {
-		alpha = 1,
-		size = util.vector2(40, 16),
-	},
-	content = ui.content { 
-		{	-- The 24 hour clock
-			name = 'text',
-			type = ui.TYPE.Text,
-			props = {
-				text = calendar.formatGameTime('%H:%M'),
-				textSize = 12,
-				textColor = util.color.rgba(1, 1, 1, 0.8),
-				anchor = util.vector2(0.5, 0.5),
-				relativePosition = util.vector2(0.5, 0.5),
-				inheritAlpha = false,
-				textShadow = true,
-				position = util.vector2(0, 0),
-			},
-		},
-	}
+local widget           = {
+    type = ui.TYPE.Image,
+    props = {
+        alpha = 1,
+        size = util.vector2(40, 16),
+    },
+    content = ui.content {
+        { -- The 24 hour clock
+            name = 'text',
+            type = ui.TYPE.Text,
+            props = {
+                text = calendar.formatGameTime('%H:%M'),
+                textSize = 12,
+                textColor = util.color.rgba(1, 1, 1, 0.8),
+                anchor = util.vector2(0.5, 0.5),
+                relativePosition = util.vector2(0.5, 0.5),
+                --inheritAlpha = false,
+                textShadow = true,
+                position = util.vector2(0, 0),
+            },
+        },
+    }
 }
 
-local wrapper = ui.create {
-	layer = 'HUD',
-	type = ui.TYPE.Container,
-	template = MWUI.templates.boxTransparent,
-	props = {
-		relativePosition = util.vector2(1, 1),
-		anchor = util.vector2(1, 1),
-		position = util.vector2(-cornerMargin + hOffset, -cornerMargin + vOffset),
-	},
-	content = ui.content {
-		{
-			template = widget
-		}
-	}
+local wrapper          = ui.create {
+    layer = 'HUD',
+    type = ui.TYPE.Container,
+    template = MWUI.templates.boxTransparent,
+    props = {
+        relativePosition = util.vector2(1, 1),
+        anchor = util.vector2(1, 1),
+        position = util.vector2(-cornerMargin + hOffset, -cornerMargin + vOffset),
+        alpha = 1,
+    },
+    content = ui.content {
+        {
+            template = widget
+        }
+    }
 }
 
 local function updateTime()
-	widget.content['text'].props.text = calendar.formatGameTime('%H:%M')
-	wrapper:update()
+    widget.content['text'].props.text = calendar.formatGameTime('%H:%M')
+    wrapper:update()
 end
 
 time.runRepeatedly(updateTime, 1 * time.minute, { type = time.GameTime })
+
+return {
+    eventHandlers = {
+        HUDTransparencyChange = function(data)
+            wrapper.layout.props.alpha = data.alpha
+            wrapper:update()
+        end,
+    }
+}
--- scripts/gold.lua
+++ scripts/gold.lua
@@ -1,37 +1,37 @@
-local ui = require('openmw.ui')
-local util = require('openmw.util')
-local self = require('openmw.self')
-local Actor = require('openmw.types').Actor
-local time = require('openmw_aux.time')
-local MWUI = require('openmw.interfaces').MWUI
-local helpers = require('scripts.helpers')
+local ui               = require('openmw.ui')
+local util             = require('openmw.util')
+local self             = require('openmw.self')
+local Actor            = require('openmw.types').Actor
+local time             = require('openmw_aux.time')
+local MWUI             = require('openmw.interfaces').MWUI
+local helpers          = require('scripts.helpers')
 
-local cornerMargin = 36
-local hOffset, vOffset  = -45, -21
-local white = ui.texture { path = 'white' }
-local gold = ui.texture { path = 'icons/m/Tx_Gold_001.tga' }
+local cornerMargin     = 36
+local hOffset, vOffset = -45, -21
+local white            = ui.texture { path = 'white' }
+local gold             = ui.texture { path = 'icons/m/Tx_Gold_001.tga' }
 
-local widget = {
-	type = ui.TYPE.Widget,
-	props = {
-		alpha = 1,
-		size = util.vector2(40, 16),
-	},
-	content = ui.content { 
-		{	-- Gold
-			name = 'text',
-			type = ui.TYPE.Text,
-			props = {
-				text = "",
-				textSize = 10,
-				textColor = util.color.rgba(1, 1, 1, 0.8),
-				anchor = util.vector2(1, 0.5),
-				relativePosition = util.vector2(1, 0.5),
+local widget           = {
+    type = ui.TYPE.Widget,
+    props = {
+        alpha = 1,
+        size = util.vector2(40, 16),
+    },
+    content = ui.content {
+        { -- Gold
+            name = 'text',
+            type = ui.TYPE.Text,
+            props = {
+                text = "",
+                textSize = 10,
+                textColor = util.color.rgba(1, 1, 1, 0.8),
+                anchor = util.vector2(1, 0.5),
+                relativePosition = util.vector2(1, 0.5),
                 position = util.vector2(-13, 0.5),
-				inheritAlpha = false,
+                --inheritAlpha = false,
                 textShadow = true,
-			},
-		},
+            },
+        },
         {
             type = ui.TYPE.Image,
             props = {
@@ -42,29 +42,39 @@ local widget = {
                 position = util.vector2(-2, -1),
             }
         },
-	}
+    }
 }
 
-local wrapper = ui.create {
-	layer = 'HUD',
-	type = ui.TYPE.Container,
+local wrapper          = ui.create {
+    layer = 'HUD',
+    type = ui.TYPE.Container,
     template = MWUI.templates.boxSolid,
-	props = {
-		relativePosition = util.vector2(1, 1),
-		anchor = util.vector2(1, 1),
-		position = util.vector2(-cornerMargin + hOffset, -cornerMargin + vOffset),
-	},
-	content = ui.content {
-		{
-			template = widget
-		}
-	}
+    props = {
+        relativePosition = util.vector2(1, 1),
+        anchor = util.vector2(1, 1),
+        position = util.vector2(-cornerMargin + hOffset, -cornerMargin + vOffset),
+        alpha = 1,
+    },
+    content = ui.content {
+        {
+            template = widget
+        }
+    }
 }
 
 local function updateTime()
-	local gold = Actor.inventory(self):countOf("Gold_001")
-	widget.content['text'].props.text = tostring(abbreviateNumber(gold))
-	wrapper:update()
+    local gold = Actor.inventory(self):countOf("Gold_001")
+    widget.content['text'].props.text = tostring(abbreviateNumber(gold))
+    wrapper:update()
 end
 
 time.runRepeatedly(updateTime, 1 * time.minute, { type = time.GameTime })
+
+return {
+    eventHandlers = {
+        HUDTransparencyChange = function(data)
+            wrapper.layout.props.alpha = data.alpha
+            wrapper:update()
+        end,
+    }
+}
